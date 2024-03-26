e4249f5e6b074b63f876e8f9d1ebfcd57be45854
Doomfang3 <saubinmathieu@gmail.com>
2024-03-26 10:34:55 +0100

    Fix path to images

--- js/obstacle.js
+++ js/obstacle.js
@@ -10,7 +10,7 @@ class Obstacle {
     this.speed = 5
     this.element = document.createElement('img')
 
-    this.element.src = '../images/redCar.png'
+    this.element.src = '/images/redCar.png'
     this.element.style.width = `${this.width}px`
     this.element.style.height = `${this.height}px`
     this.element.style.top = `${this.top}px`
